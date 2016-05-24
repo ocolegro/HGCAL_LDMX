9a2af28841d31d282fa881abd96fa03d08e9470b
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-05-24 13:54:51 -0700

    first commit

---
+++ PFCalEE/include/DetectorConstruction.hh
@@ -0,0 +1,157 @@
+#ifndef DetectorConstruction_h
+#define DetectorConstruction_h 1
+
+#include "SamplingSection.hh"
+
+#include "G4VUserDetectorConstruction.hh"
+#include "globals.hh"
+
+#include <map>
+#include <string>
+
+class G4VSolid;
+class G4Box;
+class G4LogicalVolume;
+class G4VPhysicalVolume;
+class G4Material;
+class G4UniformMagField;
+class DetectorMessenger;
+class G4Colour;
+
+/**
+ @class DetectorConstruction
+ @short builds a simple detector
+ */
+class DetectorConstruction: public G4VUserDetectorConstruction {
+public:
+
+	enum DetectorVersion {
+		v_HGCALEE_v6 = 1,
+
+	};
+
+	enum DetectorModel {
+		m_SIMPLE_20 = 0, m_SIMPLE_50 = 1, m_FULLSECTION = 2, m_SIMPLE_100 = 3,
+	};
+
+	/**
+	 @short CTOR
+	 */
+	DetectorConstruction(G4int ver = DetectorConstruction::v_HGCALEE_v6,
+			G4int mod = DetectorConstruction::m_SIMPLE_20, bool signal = false);
+
+	void buildHGCALFHE(const unsigned aVersion);
+	void buildHGCALBHE(const unsigned aVersion);
+	/**
+	 @short calorimeter structure (sampling sections)
+	 */
+	std::vector<SamplingSection> m_caloStruct;
+	std::vector<SamplingSection> *getStructure() {
+		return &m_caloStruct;
+	}
+
+	int getModel() const {
+		return model_;
+	}
+	int getVersion() const {
+		return version_;
+	}
+
+	const std::vector<G4LogicalVolume*> & getSiLogVol() {
+		return m_logicSi;
+	}
+	const std::vector<G4LogicalVolume*> & getAbsLogVol() {
+		return m_logicAbs;
+	}
+
+	/**
+	 @short define the calorimeter materials
+	 */
+	void DefineMaterials();
+	std::map<std::string, G4Material *> m_materials;
+	std::map<std::string, G4double> m_dEdx;
+	std::map<std::string, G4Colour> m_colours;
+
+	/**
+	 @short set magnetic field
+	 */
+	void SetDetModel(G4int model);
+
+	void SetMagField(G4double fieldValue);
+	G4UniformMagField* m_magField;      //pointer to the magnetic field
+
+	/**
+	 @short DTOR
+	 */
+	~DetectorConstruction();
+
+	/**
+	 @short getters
+	 */
+	G4double GetCalorSizeXY() {
+		return m_CalorSizeXY;
+	}
+	G4double GetCalorSizeZ() {
+		return m_CalorSizeZ;
+	}
+	G4double GetWorldSizeXY() {
+		return m_WorldSizeXY;
+	}
+	G4double GetWorldSizeZ() {
+		return m_WorldSizeZ;
+	}
+
+	/**
+	 @short build the detector
+	 */
+
+	G4VPhysicalVolume* Construct();
+
+private:
+
+	//detector version
+	int version_;
+	//integer to define detector model
+	int model_;
+
+	/**
+	 @short compute the calor dimensions
+	 */
+	void UpdateCalorSize();
+
+	/**
+	 @short build the calorimeter
+	 */
+	G4VPhysicalVolume* ConstructCalorimeter();
+
+	void buildSectorStack(const unsigned sectorNum, const G4double & minL,
+			const G4double & width);
+
+	void fillInterSectorSpace(const unsigned sectorNum, const G4double & minL,
+			const G4double & width);
+
+	G4VSolid *constructSolid(std::string baseName, G4double thick,
+			G4double zpos, const G4double & minL, const G4double & width,
+			const size_t which_ele);
+
+	std::vector<G4Material*> m_SensitiveMaterial;
+
+	G4double m_CalorSizeXY, m_CalorSizeZ;
+	G4double m_minRadius, m_maxRadius;
+	G4double m_maxTheta;
+	G4double m_z0pos;
+	G4double m_WorldSizeXY, m_WorldSizeZ;
+	G4double m_nSectors, m_sectorWidth, m_interSectorWidth;
+
+	G4VSolid* m_solidWorld;    //pointer to the solid World 
+	G4LogicalVolume* m_logicWorld;    //pointer to the logical World
+	G4VPhysicalVolume* m_physWorld;     //pointer to the physical World  
+
+	std::vector<G4LogicalVolume*> m_logicSi; //pointer to the logical Si volumes
+	std::vector<G4LogicalVolume*> m_logicAbs; //pointer to the logical absorber volumes situated just before the si
+
+	DetectorMessenger* m_detectorMessenger;  //pointer to the Messenger
+};
+
+#endif
+
---
+++ PFCalEE/src/PrimaryGeneratorAction.cc
@@ -0,0 +1,186 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+// $Id$
+//
+// 
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#include "PrimaryGeneratorAction.hh"
+
+#include "DetectorConstruction.hh"
+#include "PrimaryGeneratorMessenger.hh"
+
+#include "G4RunManager.hh"
+#include "G4Event.hh"
+#include "G4ParticleGun.hh"
+#include "G4ParticleTable.hh"
+#include "G4ParticleDefinition.hh"
+#include "G4SystemOfUnits.hh"
+#include "Randomize.hh"
+
+#include "HepMCG4AsciiReader.hh"
+#include "HepMCG4PythiaInterface.hh"
+
+#include <fstream>
+#include <string>
+#include <sstream>
+#define PI 3.1415926535
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+std::vector<std::string> split(const std::string &s, char delim) {
+	std::stringstream ss(s);
+	std::string item;
+	std::vector<std::string> tokens;
+	while (std::getline(ss, item, delim)) {
+		tokens.push_back(item);
+	}
+	return tokens;
+}
+
+PrimaryGeneratorAction::PrimaryGeneratorAction(G4int mod, bool signal,
+		std::string data) {
+	model_ = mod;
+	signal_ = signal;
+	data_ = data;
+	G4int n_particle = 1;
+
+	// default generator is particle gun.
+	currentGenerator = particleGun = new G4ParticleGun(n_particle);
+	currentGeneratorName = "particleGun";
+	hepmcAscii = new HepMCG4AsciiReader();
+#ifdef G4LIB_USE_PYTHIA
+	pythiaGen= new HepMCG4PythiaInterface();
+#else
+	pythiaGen = 0;
+#endif
+	gentypeMap["particleGun"] = particleGun;
+	gentypeMap["hepmcAscii"] = hepmcAscii;
+	gentypeMap["pythia"] = pythiaGen;
+
+	Detector =
+			(DetectorConstruction*) G4RunManager::GetRunManager()->GetUserDetectorConstruction();
+
+	//create a messenger for this class
+	gunMessenger = new PrimaryGeneratorMessenger(this);
+
+	// default particle kinematic
+
+	G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
+	G4String particleName;
+	G4ParticleDefinition* particle = particleTable->FindParticle(particleName =
+			"e-");
+	particleGun->SetParticleDefinition(particle);
+	particleGun->SetParticleMomentumDirection(G4ThreeVector(0., 0., 1.));
+	particleGun->SetParticleEnergy(4. * GeV);
+	G4double position = -0.5 * (Detector->GetWorldSizeZ());
+	particleGun->SetParticlePosition(G4ThreeVector(0. * cm, 0. * cm, position));
+
+	G4cout << " -- Gun position set to: 0,0," << position << G4endl;
+
+	rndmFlag = "off";
+
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+PrimaryGeneratorAction::~PrimaryGeneratorAction() {
+	delete particleGun;
+	delete hepmcAscii;
+	delete pythiaGen;
+	delete gunMessenger;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent) {
+	G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
+	G4String particleName;
+
+	G4ParticleDefinition* particle = particleTable->FindParticle(particleName =
+			"e-");
+	particleGun->SetParticleDefinition(particle);
+	if (signal_ == false) {
+		G4double et = 4.0;
+		particleGun->SetParticleEnergy(et * GeV);
+		particleGun->SetParticleMomentumDirection(G4ThreeVector(0., 0., 1.));
+	} else {
+		G4double E, dir_x, dir_y, dir_z;
+
+		std::ifstream in(data_);
+		std::string line;
+		for (int i = 0; i < anEvent->GetEventID() + 1; i++) {
+			std::getline(in, line);
+			if (i == anEvent->GetEventID()) {
+				std::vector<std::string> split_ = (split(line, ' '));
+				//Fetch the components from this line
+				std::istringstream os(split_.at(0));
+				os >> E;
+				os.clear();
+
+				os.str(split_.at(1));
+				os >> dir_x;
+				os.clear();
+
+				os.str(split_.at(2));
+				os >> dir_y;
+				os.clear();
+
+				os.str(split_.at(3));
+				os >> dir_z;
+				os.clear();
+			}
+		}
+		particleGun->SetParticleEnergy(E * GeV);
+		particleGun->SetParticleMomentumDirection(
+				G4ThreeVector(dir_x, dir_y, dir_z));
+	}
+
+	G4double y0 = G4RandFlat::shoot(-65.,65);
+	G4double x0 = G4RandFlat::shoot(-65.,65);
+	G4double z0 = -0.5 * (Detector->GetWorldSizeZ());
+
+	if (model_ == 0)
+		particleGun->SetParticlePosition(G4ThreeVector(x0, y0, z0));
+
+	G4cout << " -- Gun position set to: " << x0 << "," << y0 << "," << z0
+			<< G4endl;
+
+	if (currentGenerator) {
+		currentGenerator->GeneratePrimaryVertex(anEvent);
+
+	} else
+		G4Exception("PrimaryGeneratorAction::GeneratePrimaries",
+				"PrimaryGeneratorAction001", FatalException,
+				"generator is not instanciated.");
+
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
