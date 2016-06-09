caabeef80d44dfb836e9f205ddefb97d5ac851c7
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-06-09 16:44:15 -0700

    debugging

--- PFCalEE/src/SteppingAction.cc
+++ PFCalEE/src/SteppingAction.cc
@@ -61,9 +61,8 @@ void SteppingAction::UserSteppingAction(const G4Step* aStep) {
 
 		}
 		const std::vector<const G4Track*>* secondaryTracks = aStep->GetSecondaryInCurrentStep();
-
+			std::cout << "The size of secondaryTracks->size() is " << secondaryTracks->size() << std::endl;
 			for (unsigned iT(0); iT < secondaryTracks->size() < iT; iT++){
-
 				const G4Track* sTrack = secondaryTracks->at(iT);
 				G4double sKinEng = sTrack->GetKineticEnergy();
 				G4int sTrackID = sTrack->GetTrackID();
