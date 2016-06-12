a3320b9e5e4c21fa8a276a0a4c3a7dc6b7db489e
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-06-11 19:37:40 -0700

    first commit

--- PFCalEE/submitProd.py
+++ PFCalEE/submitProd.py
@@ -33,16 +33,16 @@ label=''
 nevents=opt.nevts
 myqueue=opt.queue
 
-thickness_ = [1,2]
+thickness_ = [5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,105,110,115,120,125]
 for thickness in thickness_:
     outDir='%s/git_%s/version_%d/model_%d'%(opt.out,opt.gittag,opt.version,opt.model)
     outDir='%s/%s'%(outDir,label)
     eosDir='%s/git%s'%(opt.eos,opt.gittag)
     outDir='%s/particle_%s/'%(outDir,opt.particle)
     if (opt.run>=0) : outDir='%s/run_%d/zthick_%s/'%(outDir,opt.run,thickness)
-
+    
     os.system('mkdir -p %s'%outDir)
-
+    
     #wrapper
     scriptFile = open('%s/runJob.sh'%(outDir), 'w')
     scriptFile.write('#!/bin/bash\n')
@@ -52,7 +52,7 @@ for thickness in thickness_:
     scriptFile.write('PFCalEE g4steer.mac %d %d %s %d %s| tee g4.log\n'%(opt.version,opt.model,thickness,opt.run,opt.data))
     outTag='%s_version%d_model%d'%(label,opt.version,opt.model)
     if (opt.run>=0) : outTag='%s_run%d'%(outTag,opt.run)
-    scriptFile.write('mv PFcal.root HGcal_%s.root\n'%(outTag))
+    scriptFile.write('mv PFcal.root HGcal_%s_%s.root\n'%(outTag,thickness))
     scriptFile.write('localdir=`pwd`\n')
     scriptFile.write('echo "--Local directory is " $localdir >> g4.log\n')
     scriptFile.write('ls * >> g4.log\n')
@@ -60,11 +60,11 @@ for thickness in thickness_:
         scriptFile.write('grep "alias eos=" /afs/cern.ch/project/eos/installation/cms/etc/setup.sh | sed "s/alias /export my/" > eosenv.sh\n')
         scriptFile.write('source eosenv.sh\n')
         scriptFile.write('$myeos mkdir -p %s\n'%eosDir)
-        scriptFile.write('$myeos cp HGcal_%s.root /eos/cms%s/HGcal_%s.root\n'%(outTag,eosDir,outTag))
+        scriptFile.write('$myeos cp HGcal_%s_%s.root /eos/cms%s/HGcal_%s_%s.root\n'%(outTag,thickness,eosDir,outTag,thickness))
         scriptFile.write('if (( "$?" != "0" )); then\n')
         scriptFile.write('echo " --- Problem with copy of file PFcal.root to EOS. Keeping locally." >> g4.log\n')
         scriptFile.write('fi\n')
-        scriptFile.write('rm HGcal_%s.root\n'%(outTag))
+        scriptFile.write('rm HGcal_%s_%s.root\n'%(outTag,thickness))
 
 
 
@@ -74,8 +74,8 @@ for thickness in thickness_:
     scriptFile.write('echo "All done"\n')
     scriptFile.close()
 
-    #write geant 4 macro
-    g4Macro = open('%s/g4steer.mac'%(outDir), 'w')
+#write geant 4 macro
+g4Macro = open('%s/g4steer.mac'%(outDir), 'w')
     g4Macro.write('/control/verbose 0\n')
     g4Macro.write('/control/saveHistory\n')
     g4Macro.write('/run/verbose 0\n')
@@ -85,9 +85,10 @@ for thickness in thickness_:
     g4Macro.write('/random/setSeeds %d %d\n'%( random.uniform(0,100000), random.uniform(0,100000) ) )
     g4Macro.write('/run/beamOn %d\n'%(nevents))
     g4Macro.close()
-
+    
     #submit
     os.system('chmod u+rwx %s/runJob.sh'%outDir)
     if opt.nosubmit : os.system('LSB_JOB_REPORT_MAIL=N echo bsub -q %s -N %s/runJob.sh'%(myqueue,outDir))
     else: os.system("LSB_JOB_REPORT_MAIL=N bsub -q %s -N \'%s/runJob.sh\'"%(myqueue,outDir))
-
+        
+        93,0-1        Bot
