41bab37658d42b5f6e3bc4c7291b1c8bf00b4155
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-06-22 16:56:32 -0700

    up

--- PFCALEE/submitProdSecond.py
+++ PFCALEE/submitProdSecond.py
@@ -72,7 +72,7 @@ for thickness in thickness_:
     scriptFile.close()
 
 	#write geant 4 macro
-	g4Macro = open('%s/g4steer.mac'%(outDir), 'w')
+    g4Macro = open('%s/g4steer.mac'%(outDir), 'w')
     g4Macro.write('/control/verbose 0\n')
     g4Macro.write('/control/saveHistory\n')
     g4Macro.write('/run/verbose 0\n')
