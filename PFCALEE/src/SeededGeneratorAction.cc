85e5389fa1e2e41abe6aa537fce27ae7bb78c4eb
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-06-23 11:27:12 -0700

    some improvements and fixes

--- PFCALEE/src/SeededGeneratorAction.cc
+++ PFCALEE/src/SeededGeneratorAction.cc
@@ -132,7 +132,7 @@ void SeededGeneratorAction::GeneratePrimaries(G4Event* anEvent) {
 			"e-");
 	particleGun->SetParticleDefinition(particle);
 	int currentEvt = anEvent->GetEventID();
-	tree_->GetEntry(currentEvt);
+	tree_->GetEntry(currentEvt-1);
 	G4double et = 0.0;
 	PipeData();
 	CLHEP::HepRandom::restoreEngineStatus ("temp.rndm");
