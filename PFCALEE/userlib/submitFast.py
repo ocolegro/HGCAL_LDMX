2d8ad42dc793efa31ba3177d919e5b8e77e26aff
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-06-23 23:30:44 -0700

    new

--- PFCALEE/userlib/submitFast.py
+++ PFCALEE/userlib/submitFast.py
@@ -40,7 +40,7 @@ else:
     eosDir='%s/'%(outDir)
     eosDirIn = '%s/'%(outDir)
 outTag='%s_version%d_model%d'%(label,opt.version,opt.model)
-if (opt.run>=0) : outTag='%s_run%d_second_v3'%(outTag,opt.run)
+if (opt.run>=0) : outTag='%s_thick_1_run%d_second_v3'%(outTag,opt.run)
 
 print 'The eosdir is ' + str(eosDir)
 print 'The eosdirIn is ' + str(eosDirIn)
