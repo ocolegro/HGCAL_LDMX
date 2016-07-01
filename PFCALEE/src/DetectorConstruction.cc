ced5a417e0947903810ee9d67d5fa819bf5c9f48
Owen Colegrove <ocolegro@dhcp-visitor-216-133.slac.stanford.edu>
2016-07-01 16:25:10 -0700

    first geom test

--- PFCALEE/src/DetectorConstruction.cc
+++ PFCALEE/src/DetectorConstruction.cc
@@ -619,7 +619,7 @@ G4VSolid *DetectorConstruction::constructSolid(std::string baseName,
 			solid = new G4Polyhedra(baseName + "box",
 					0, 2 * pi,
 					6, 2,
-					{0,5},{0,5}); // width / 2, m_CalorSizeXY / 2,thick / 2);
+					{0,5},{0,0},{20,20}); // width / 2, m_CalorSizeXY / 2,thick / 2);
 
 		}
 
