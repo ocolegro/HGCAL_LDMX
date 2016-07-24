a73a41359628d30032f0f94c69590e54a4c78ed7
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-07-24 13:40:20 -0700

    iup

--- PFCALEE/src/PrimaryGeneratorAction.cc
+++ PFCALEE/src/PrimaryGeneratorAction.cc
@@ -134,9 +134,9 @@ void PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent) {
 	G4ParticleDefinition* particle = particleTable->FindParticle(particleName =
 			"e-");
 	particleGun->SetParticleDefinition(particle);
-	G4double et = 4.0;
-	//double engs3[16] = {.25,.5,.75,1.,1.25,1.5,1.75,2,2.25,2.5,2.75,3.,3.25,3.5,3.75,4.0};
-	//unsigned iSecret = rand() % 16 ;
+	//G4double et = 4.0;
+	double engs3[16] = {.25,.5,.75,1.,1.25,1.5,1.75,2,2.25,2.5,2.75,3.,3.25,3.5,3.75,4.0};
+	unsigned iSecret = rand() % 16 ;
 	particleGun->SetParticleEnergy(et * GeV);
 	particleGun->SetParticleMomentumDirection(G4ThreeVector(0., 0., 1.));
 
