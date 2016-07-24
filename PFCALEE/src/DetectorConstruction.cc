6bcada616cb0a5a8e670d5241402a61cec47a709
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-07-24 12:15:42 -0700

    iup

--- PFCALEE/src/DetectorConstruction.cc
+++ PFCALEE/src/DetectorConstruction.cc
@@ -618,13 +618,13 @@ G4VSolid *DetectorConstruction::constructSolid(std::string baseName,
 			G4double hexaRad = 101.59994;//78.0;
 			//G4double cos30 = 0.86602540378;
 			G4double a[2] = {0,thick/2.0},b[2] ={0,0},c[2] = {hexaRad,hexaRad};
-			solid = new G4Polyhedra(baseName + "box",
+			/*solid = new G4Polyhedra(baseName + "box",
 							0, 2 * pi,
 							6, 2,
-							a,b,c);
+							a,b,c);*/
 			//solid = new G4Box(baseName + "box", width / 2, m_CalorSizeXY / 2,
 			//		thick / 2);
-			/*
+
 			G4VSolid* sHexa = new G4Polyhedra(baseName + "box",
 							0, 2 * pi,
 							6, 2,
@@ -648,8 +648,8 @@ G4VSolid *DetectorConstruction::constructSolid(std::string baseName,
 					rot,
 					trans.rotate(i*3.14/3,zAxis ));
 			}
-*/
-			//solid = sHexa;// (G4VSolid*)sUnion; // sUnion;
+
+			solid = sUnion;//sHexa;// (G4VSolid*)sUnion; // sUnion;
 			/*
  			double layerR = tan(m_maxTheta) * (zpos - m_z0pos);
  			solid = new G4Tubs(baseName + "box", 0, layerR, thick / 2, minL,width);*/
