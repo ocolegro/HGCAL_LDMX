69a170f1979cc527dcbce562cbd3fd94df4f3c99
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-07-23 18:44:32 -0700

    ipu

--- PFCALEE/src/DetectorConstruction.cc
+++ PFCALEE/src/DetectorConstruction.cc
@@ -624,7 +624,7 @@ G4VSolid *DetectorConstruction::constructSolid(std::string baseName,
 									6, 2,
 									a,b,c);
 			G4RotationMatrix* rot = new G4RotationMatrix(0,0,0);
-			const G4ThreeVector trans= G4ThreeVector(20,20,0);
+			const G4ThreeVector trans= G4ThreeVector(40,0,0);
 			G4UnionSolid* s1us2 = new G4UnionSolid(baseName + "box",
 					s1,
 					s2,
