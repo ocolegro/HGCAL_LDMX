45646960e92846166b0ee1adb0b4b49732d72901
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-07-24 10:29:25 -0700

    iup

--- PFCALEE/src/DetectorConstruction.cc
+++ PFCALEE/src/DetectorConstruction.cc
@@ -614,7 +614,7 @@ G4VSolid *DetectorConstruction::constructSolid(std::string baseName,
 		}
 	} else {
 		if (model_ == DetectorConstruction::m_FULLSECTION) {
-			G4double hexaRad = 78.0;
+			G4double hexaRad = 101.59994;//78.0;
 			//G4double cos30 = 0.86602540378;
 			G4double a[2] = {0,thick},b[2] ={0,0},c[2] = {hexaRad,hexaRad};
 
