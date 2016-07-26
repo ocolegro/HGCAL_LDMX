2c60b4d78bb051d481dc3b1034544659220d56df
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-07-26 15:24:48 -0700

    iup

--- PFCALEE/src/SamplingSection.cc
+++ PFCALEE/src/SamplingSection.cc
@@ -63,7 +63,7 @@ std::pair<G4bool,G4bool> SamplingSection::add(G4double depositRawE,G4VPhysicalVo
 			unsigned eleidx = ie % n_elements;
 			isSens = isSensitiveElement(eleidx);
 			sublayer_RawDep[eleidx] += depositRawE;
-			/*//if (eventAction_->firstPass() == false){
+			//if (eventAction_->firstPass() == false){
 				G4SiHit lHit;
 				//std::cout << "The deposited raw energy is getting stored " << std::endl;
 				lHit.energyDep = depositRawE;
@@ -79,7 +79,7 @@ std::pair<G4bool,G4bool> SamplingSection::add(G4double depositRawE,G4VPhysicalVo
 				lHit.hit_z = position.z();
 				//std::cout << "Processing an event" << std::endl;
 				sens_HitVec[idx].push_back(lHit);
-			//}*/
+			//}
 
 			} //if in right material
 		} //loop on available materials
