76a65b7725762c53ff562a4c75ab8550f6dcd5e5
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-08-05 08:36:13 -0700

    upup

--- PFCALEE/userlib/submitAnalyzeLPC.py
+++ PFCALEE/userlib/submitAnalyzeLPC.py
@@ -63,7 +63,7 @@ for thickness in thickness_:
     if (opt.fast>0) : outTag = '%s_second' % (outTag)
 
     if opt.nosubmit   : scriptFile.write('./%s root://cmseos.fnal.gov//%s/HGcal_%s.root  \n'%(opt.macro,outDir,outTag))
-    else: scriptFile.write('./%s HGcal_%s.root  \n'%(opt.macro,outDir,outTag))
+    else: scriptFile.write('./%s HGcal_%s.root  \n'%(opt.macro,outTag))
 
     #scriptFile.write('xrdcp -f analyzed_tuple.root root://cmseos.fnal.gov/%s/analyzed_%s.root\n'%(outDir,outTag))
 
